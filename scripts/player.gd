9b647d7ee45236c2578289893a89935437013097
Manav <30342951+MzaxnaV@users.noreply.github.com>
2024-05-03 18:10:24 +0100

    add animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,15 +5,21 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var wasInAir = false
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		wasInAir = true
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -24,3 +30,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	if wasInAir and is_on_floor():
+		wasInAir = false
+		$AnimatedSprite2D.play("idle")
